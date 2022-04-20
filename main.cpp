34aa56d6cc2043dfa714309b60da91ae1c45c18f
smlunar <61670949+moon0G@users.noreply.github.com>
2022-04-20 15:16:59 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,7 @@ int main() {
     std::cin>>op;
     std::cout<<"\n";
 
-    std::cout<<"The first number will be affected (by the operator) with the numbers entered after it\n(n[0]=1, n[1]=1, n[2]=1 therefore 1-1-1 = -1)"<<std::endl;
-    std::cout<<"(Inputing 0 will stop input)\nInput Numbers: "<<std::endl;   
+    std::cout<<"The first number will be affected (by the operator) with the numbers entered after it\n(n[0]=1, n[1]=1, n[2]=1 therefore 1-1-1 = -1)\n(Inputing 0 will stop input)\nInput Numbers: "<<'\n';   
     while(num!=0) {
         std::cin>>num;
         if(num!=0) {
@@ -20,7 +19,7 @@ int main() {
         }
     }
     int ans = do_op(op, a);
-    std::cout<<ans<<std::endl;
+    std::cout<<ans<<'\n';
 }
 
 
